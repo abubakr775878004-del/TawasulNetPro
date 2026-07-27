cf19fbd84557ccfcbcc929f0cf12371a36cb2d54
abubakr775878004-del <abubakr775878004@gmail.com>
2026-07-27 19:26:10 +0300

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // <-- أضف هذا السطر هنا لحل مشكلة الصفحة البيضاء نهائياً
+  base: './',
   server: {
     host: "::",
     port: 8080,
